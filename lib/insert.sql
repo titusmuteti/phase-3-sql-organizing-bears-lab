9f363f73a660c6822282b06fbd7e1d224b94ef5a
Titus Mumo <titusmumo@gmail.com>
2022-10-27 18:05:18 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr.Chocolate", 10, "F", "Brown", "Sanguine", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 2, "M", "Black", "Sanguine", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 20, "M", "Black", "melanolic", "1");
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 8, "F", "Brown", "choleric", "0");
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 4, "F", "Black", "Sanguine", "1");
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 25, "M", "Brown", "choleric", "0");
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 3, "F", "Red", "Phlegmatic", "1");
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 15, "F", "Blue", "Sanguine", "1");
